985ff07ce499e904df912dbf7f31a7b6056a9cfe
Jeanne-XoX <jeannedormoy@hotmail.com>
2025-03-05 18:57:27 -0500

    UI level select fini !!!! 🐶

--- My project/Assets/Script/Jeanne/MainMenu.cs
+++ My project/Assets/Script/Jeanne/MainMenu.cs
@@ -1,8 +1,26 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public Button[] boutons;
+
+    //Gere les niveaux cliquables
+    private void Awake()
+    {
+        int unlockedLevel = PlayerPrefs.GetInt("UnlockedLevel", 1);
+        for (int i = 0; i < boutons.Length; i++)
+        {
+            boutons[i].interactable = false;
+        }
+        for (int i = 0; i < unlockedLevel; i++)
+        {
+            boutons[i].interactable = true;
+        }
+
+    }
+
 
     public void Jouer()
     {
@@ -29,6 +47,7 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene("UI_Accueil");
     }
 
+    //Load le niveau quon clique
     public void OuvreNiveaux(int niveauId)
     {
         string nomNiveau = "Niveau" + niveauId;
--- My project/Assets/Script/Jeanne/Victory.cs
+++ My project/Assets/Script/Jeanne/Victory.cs
@@ -15,11 +15,25 @@ public class Victory : MonoBehaviour
 
     }
 
+    //Pop le UI de victoire 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             SceneManager.LoadScene("Victoire");
+            UnlockNewLevel();
         } 
     }
+
+    //Unlock un nouveau niveau 
+    void UnlockNewLevel()
+    {
+        if (SceneManager.GetActiveScene().buildIndex < PlayerPrefs.GetInt("ReachedIndex"))
+        {
+            PlayerPrefs.SetInt("ReachedIndex", SceneManager.GetActiveScene().buildIndex +1);
+            PlayerPrefs.SetInt("UnlockedLevel", PlayerPrefs.GetInt("UnlockedLevel", 1)+1);
+            PlayerPrefs.Save();
+        }
+    }
+
 }
