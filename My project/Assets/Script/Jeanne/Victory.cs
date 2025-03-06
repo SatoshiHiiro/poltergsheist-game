ef7a30cd9bee33a9d0981306134b76d4e1a6a4d0
Jeanne-XoX <jeannedormoy@hotmail.com>
2025-03-06 01:02:30 -0500

    idk

--- My project/Assets/Script/Jeanne/Victory.cs
+++ My project/Assets/Script/Jeanne/Victory.cs
@@ -31,7 +31,7 @@ public class Victory : MonoBehaviour
         if (SceneManager.GetActiveScene().buildIndex < PlayerPrefs.GetInt("ReachedIndex"))
         {
             PlayerPrefs.SetInt("ReachedIndex", SceneManager.GetActiveScene().buildIndex +1);
-            PlayerPrefs.SetInt("UnlockedLevel", PlayerPrefs.GetInt("UnlockedLevel", 1)+1);
+            PlayerPrefs.SetInt("UnlockedLevel", PlayerPrefs.GetInt("UnlockedLevel", 1) +1);
             PlayerPrefs.Save();
         }
     }
