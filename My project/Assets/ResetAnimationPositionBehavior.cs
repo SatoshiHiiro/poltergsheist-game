f9f26da18a896eb2bd1cbb49fe66fc9ed17b994f
SatoshiHiiro <axiocrafting@gmail.com>
2025-04-05 15:57:42 -0500

    Animator maybe final fix today

--- My project/Assets/ResetAnimationPositionBehavior.cs
+++ My project/Assets/ResetAnimationPositionBehavior.cs
@@ -1,15 +1,12 @@
 using UnityEngine;
 
-public class PositionReset : StateMachineBehaviour
+public class ResetAnimationPositionBehavior : StateMachineBehaviour
 {
-    Vector3 iniPos;
-    bool isFirstTime;
-
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-
-    }
+    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //
+    //}
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
@@ -20,14 +17,14 @@ public class PositionReset : StateMachineBehaviour
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-
+        animator.rootPosition = animator.GetComponent<HeightAndSpriteResizeSystem>().heightPos + animator.transform.parent.position;
     }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
-    override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-
-    }
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
 
     // OnStateIK is called right after Animator.OnAnimatorIK()
     //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
