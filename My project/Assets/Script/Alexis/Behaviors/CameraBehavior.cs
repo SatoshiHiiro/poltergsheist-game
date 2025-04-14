2b4b53907e11eb4b729c4a412a87cd7afdf32aaf
SatoshiHiiro <axiocrafting@gmail.com>
2025-04-13 22:04:25 -0500

    Actual CameraBehavior Fix

--- My project/Assets/Script/Alexis/Behaviors/CameraBehavior.cs
+++ My project/Assets/Script/Alexis/Behaviors/CameraBehavior.cs
@@ -62,24 +62,30 @@ public class CameraBehavior : MonoBehaviour
     {
         Vector3 currentPos = transform.position;
         Vector3 focusPos = focus.position;
+        float yMinAdjust = minY + halfSizeY;
+        float yMaxAdjust = maxY - halfSizeY;
+        float xMinAdjust = minX + halfSizeX;
+        float xMaxAdjust = maxX - halfSizeX;
 
         //Gestion des limites et du mouvement de la caméra sur y
         halfSizeY = cam.orthographicSize;
         posSelf.y = Mathf.Lerp(currentPos.y, focusPos.y, camSpeed * Time.deltaTime);
-        posSelf.y = Mathf.Clamp(posSelf.y, minY + halfSizeY, maxY - halfSizeY);
+        posSelf.y = Mathf.Clamp(posSelf.y, yMinAdjust, yMaxAdjust);
 
-        if (halfSizeY * 2 > maxY - minY && focusPos.y > minY + halfSizeY)
+        if (halfSizeY * 2 > maxY - minY && (focusPos.y > yMinAdjust || focusPos.y < yMaxAdjust))
         {
-            if (focusPos.y > minY + halfSizeY)
-            {
-                posSelf.y = currentPos.y;
-            }
+            posSelf.y = yMinAdjust;
         }
 
         //Gestion des limites et du mouvement de la caméra sur x
         halfSizeX = halfSizeY * cam.aspect;
         posSelf.x = Mathf.Lerp(currentPos.x, focusPos.x, camSpeed * Time.deltaTime);
         posSelf.x = Mathf.Clamp(posSelf.x, minX + halfSizeX, maxX - halfSizeX);
+
+        if (halfSizeX * 2 > maxX - minX && (focusPos.x > xMinAdjust || focusPos.y < xMaxAdjust))
+        {
+            posSelf.x = xMinAdjust;
+        }
 
         transform.position = posSelf;
     }
