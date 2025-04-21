cafc75a380ed74db0c80f610f6db24e38608427e
Lilou18 <60436875+Lilou18@users.noreply.github.com>
2025-04-21 16:16:38 -0400

    fix suspicion bar rtpc

--- My project/Assets/Script/SuspicionUI.cs
+++ My project/Assets/Script/SuspicionUI.cs
@@ -6,17 +6,22 @@ public class SuspicionUI : MonoBehaviour
     // This class manage the suspicion UI.
 
     [SerializeField] private Slider suspicionSlider;
+    //[SerializeField] public AK.Wwise.Event suspicionBar;
+    [SerializeField] protected AK.Wwise.RTPC suspicionBarRTCP;
 
     private void Start()
     {
         suspicionSlider.value = 0;
         SuspicionManager.Instance.OnSuspicionChanged += UpdateUI;
+        //suspicionBar.Post(gameObject);
     }
 
     private void UpdateUI(float suspiciousAmount)
     {
         //print("UI CHANGE");
         suspicionSlider.value = suspiciousAmount;
+        suspicionBarRTCP.SetValue(null, suspiciousAmount);
+        print(suspiciousAmount);
     }
 
     private void OnDisable()
