f35487748877ff789736c807bdbbfd6034f3da7c
Lilou18 <60436875+Lilou18@users.noreply.github.com>
2025-04-24 21:36:30 -0400

    add audiosettings pauseMenu

--- My project/Assets/Script/Jeanne/InGameMenu.cs
+++ My project/Assets/Script/Jeanne/InGameMenu.cs
@@ -1,26 +1,41 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using static UnityEngine.Rendering.DebugUI;
 
 public class InGameMenu : MonoBehaviour
 {
+    [SerializeField] GameObject panel;
     public GameObject optionsMenu;
+    [SerializeField] public GameObject audioSettings;
+    bool isVisible;
+    public static bool isGamePaused;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        isVisible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         // active ou desactive le overlay quand on clique sur escape
-        if (Input.GetKeyDown(KeyCode.Escape)) 
+        if (Input.GetKeyDown(KeyCode.Escape))
         { 
-            optionsMenu.gameObject.SetActive(!optionsMenu.gameObject.activeSelf);
+            isVisible = !isVisible;
+            isGamePaused = !isGamePaused;
+            panel.SetActive(isVisible);
+            optionsMenu.SetActive(isVisible);
+            //print("ISVISIBLE" +isVisible);
+
+            if(isVisible == false)
+            {
+                audioSettings.SetActive(false);
+            }
 
             // met en pause le jeu quand le overlay est actif 
-            Time.timeScale = optionsMenu.activeSelf ? 0f : 1f;
-            if (optionsMenu.activeSelf)
+            Time.timeScale = isVisible ? 0f : 1f;
+            if (isVisible)
             {
                 AkUnitySoundEngine.Suspend();
             }
